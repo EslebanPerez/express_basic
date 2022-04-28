ac07fe074bc1814215bc7d2eda969db8cd89dd1b
EslebanPerez <eslebanperez@gmail.com>
2022-04-28 01:31:28 -0500

    7. Agregando una nueva ruta que regresa un objeto

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get('/launch',(req,res) =>{
     res.send('Bienvenidos a LaunchX')
 })
 
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorerIinNode', (req, res) =>{
+    const explorer = { name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
